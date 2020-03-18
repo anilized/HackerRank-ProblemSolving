3beaf8531c20216f1ef18b64adf484f12cedda26
anilcan <anillcan7@gmail.com>
2020-03-18 19:01:23 +0300

    Repeated Strings Solution

---
+++ repeated-strings.py
@@ -0,0 +1,16 @@
+# return the count
+# count s[:mod]
+
+def repeatedString(s, n):
+    length = len(s)
+    mod = n % length
+    times = n // length
+    count = s.count('a')
+    return (count * times) + s[:mod].count('a') 
+    
+
+
+s = "aba"
+n = 10
+
+print(repeatedString(s,n))
